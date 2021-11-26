1c25558165d0fbbe6ee2545f6f5f35e054c10cf3
Sun <solskinartwork@gmail.com>
2021-11-25 22:27:35 -0500

    adding content to view

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ var locations = [];
 
 
 //get weather using api
-function getWeather(spot){
-    let city = spot || document.getElementById(searchText).value();
-    document.querySelector("#searchText").value = "";
-    let getApi = '';
+function getWeather(){
+    let city = $("#searchText").val();
+    
+    var key = "fd761b5adafdbd34108c498e887235cd";
+    let getApi = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+ key;
     fetch(getApi)
 
     .then((res)=>res.json())
     .then (data =>{
         $(".weatherInfo").empty();
-        $(".weatherInfo").append('<section> id ="box1" class="box1"><div id = "city" class= "title is -2"> ${data.city.name} </div></section>')
-        document.getElementById("city").textContent = data.city.name;
+        $(".weatherInfo").append(`<section id ="box1" class="box1"><div id = "city" class= "title is -2"> ${data.main.temp} </div></section>`)
+        $(".weatherInfo").append(`<section id ="box1" class="box1"><div id = "city" class= "title is -2"> ${data.main.humidity} </div></section>`)
+        $(".weatherInfo").append(`<section id ="box1" class="box1"><div id = "city" class= "title is -2"> ${data.wind.speed} </div></section>`)
+        $(".weatherInfo").append(`<section id ="box1" class="box1"><div id = "city" class= "title is -2"> ${data.name} </div></section>`)
+  
         console.log(data);
     });
 }
@@ -55,7 +59,7 @@ function clearMainDay(){
     $(".weatherInfo").empty();
 }
 
-document.getElementById("searchForm").addEventListener("click",function(event){
+document.getElementById("searchBtn").addEventListener("click",function(event){
     clearMainDay();
     console.log("Button has been clicked")
     getWeather();
